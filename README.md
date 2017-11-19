2808ee6190c08f09594453e8ef97cc63a1159187
Cyrille Pontvieux <cpontvieux@systra.com>
2017-11-20 00:49:55 +0100

    New version that uses original mapboxgl on method

--- README.md
+++ README.md
@@ -3,10 +3,8 @@
 The current implementation of `mouseenter` and `mouseleave` events in *mapbox-gl* are a little buggy:
 - a `mouseenter` event is generated on a layer *X* even if layer *Y* is above layer *X* on the enter region.
 - a `mouseleave` event is generated on a layer *Y* if layer *Y* is above layer *X* and the mouse is moved when in a region that belongs to both *X* and *Y*.
-- the bubble effect found commonly in the DOM is not respected (stopPropagation for instance).
+- the [*bubble effect*](https://developer.mozilla.org/en-US/docs/Web/API/Event/bubbles) found commonly in the DOM is not respected ([`stopPropagation`](https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Examples#Example_5:_Event_Propagation) for instance).
 
-This plugin attempt to correct those misbehaviors by providng the following new method on `map`:
-- `onMouseOver(layerId, enterCallback, exitCallback, moveOnCallback)`
-- `onClick(layerId, callback)`
+This plugin attempt to correct those misbehaviors but does not implement [`stopImmediatePropagation`](https://developer.mozilla.org/en-US/docs/Web/API/Event/stopImmediatePropagation).
 
 For more info, see the [documentation API](https://jrd.github.io/mapbox-gl-mouse-over-event/)
--- index.js
+++ index.js
@@ -18,85 +18,234 @@
         root.mapboxgl = factory(root.mapboxgl);
     }
 }(typeof self !== "undefined" ? self : this, function (mapboxgl) {
-    let addFeatureOnEvent = e => {
-        if (e.features && e.features.length) {
-            e.feature = e.features[0];
-        }
-    };
+    const isInteractiveEvent = eventType => eventType && (
+        eventType.startsWith("mouse") ||
+        eventType.startsWith("touch") ||
+        ["click", "dblclick", "contextmenu"].includes(eventType)
+    );
 
     /**
      * @exports mapboxgl-Map
      */
-    const GLMap = mapboxgl.Map;
-
-    let getFeatureId = feature => feature.id || (feature.properties && feature.properties.id);
+    const Map = mapboxgl.Map;
+    const originalMapOnFunc = Map.prototype.on;
+    const originalMapOffFunc = Map.prototype.off;
+    const originalMapFireFunc = Map.prototype.fire;
 
     /**
-     * Create a mouse over effect on a layer by using callbacks.
+     * Add a listener to the map concerning the layer `layerId`.
+     * The listener should test whether the specific event is for the layer or not.
      *
-     * `callback` functions is the same as for `on` map method.
-     * Each *callback* is **optional**.
-     *
-     * Each *callback* `event` have a new attribute **`feature`** which is just a shorthand for `features[0]`.
-     * @alias module:mapboxgl-Map#onMouseOver
-     * @param {string} layerName name of the layer
-     * @param {callback} enterCallback callback method when entering a feature on the layer
-     * @param {callback} exitCallback callback method when leaving feature on the layer
-     * @param {callback} moveOnCallback callback method when moving on a feature on the layer
+     * Events will be fired from top to bottom (following pseudo-z-index)
+     * and only if the [`originalEvent.cancelBubble` is falsy](https://developer.mozilla.org/en-US/docs/Web/API/Event/cancelBubble).
+     * @param   {string}   layerId   the layer id.
+     * @param   {string}   eventType the event type.
+     * @param   {function} listener  the listener which will be called with a mapbox event.
+     * @returns {function} a wrapped listener, original listener could be found in the `originalListener` property.
      */
-    GLMap.prototype.onMouseOver = function (layerName, enterCallback = () => {}, exitCallback = () => {}, moveOnCallback = () => {}) {
-        let lastFeatureId = null;
-        this.on("mousemove", layerName, e => {
-            if (!e.originalEvent.cancelBubble) {
-                addFeatureOnEvent(e);
-                let featureOnMap = this.queryRenderedFeatures(e.point)[0];
-                let featureOnLayer = e.feature;
-                let featureOnMapId = getFeatureId(featureOnMap);
-                let featureOnLayerId = getFeatureId(featureOnLayer);
-                if (featureOnMapId == featureOnLayerId) {
-                    if (featureOnLayerId != lastFeatureId) {
-                        lastFeatureId = featureOnLayerId;
-                        enterCallback(e);
-                    } else {
-                        moveOnCallback(e);
+    Map.prototype.addListenerForLayer = function (layerId, eventType, listener) {
+        this._layersEvented = this._layersEvented || [];
+        let evented = this._layersEvented[layerId] = this._layersEvented[layerId] || new mapboxgl.Evented();
+        let wrappedListener = function (event) {
+            (!event.originalEvent.bubbles || !event.originalEvent.cancelBubble) && listener.call(this, event);
+        };
+        wrappedListener.originalListener = listener;
+        evented.on(eventType, wrappedListener);
+        return wrappedListener;
+    };
+
+    /**
+     * Get all (wrapped) listeners for the specified layer id.
+     * @param   {string} layerId the layer id
+     * @returns {object} object with all listeners as property.
+     *
+     * `Evented._listeners` or empty object.
+     * @see [`Evented` on *mapboxgl*](https://www.mapbox.com/mapbox-gl-js/api/#evented).
+     */
+    Map.prototype.getListenersForLayer = function (layerId) {
+        let layersEvented = this._layersEvented || [];
+        let evented = layersEvented[layerId] || null;
+        return evented && evented._listeners || {};
+    };
+
+    /**
+     * Remove a listener from a layer.
+     * The listener could be a real or a wrapper listener.
+     * @param   {string}   layerId   the layer id corresponding to the listener.
+     * @param   {string}   eventType the event type.
+     * @param   {function} listener  the listener to remove.
+     */
+    Map.prototype.removeListenerFromLayer = function (layerId, eventType, listener) {
+        let evented = this._getLayerEventedForLayer(layerId) || null;
+        let listeners = evented && evented._listeners && evented._listeners[eventType] || [];
+        let wrappedListener = listeners.find(l => l === listener || l.originalListener === listener);
+        if (wrappedListener) {
+            evented.off(eventType, wrappedListener);
+        }
+    };
+
+    Map.prototype._getLayerEventedForLayer = function (layerId) {
+        return this._layersEvented && this._layersEvented[layerId];
+    };
+
+    Map.prototype._getLayersEventedByZIndexFor = function (type) {
+        let layersEvented = [];
+        let layersId = this.style && this.style._order && this.style._order.slice().reverse() || [];
+        for (let layerId of layersId) {
+            let layerEvented = this._getLayerEventedForLayer(layerId);
+            if (layerEvented && layerEvented.listens(type)) {
+                layersEvented.push(layerEvented);
+            }
+        }
+        return layersEvented;
+    };
+
+    /**
+     * Enhance `on` method for registering an event.
+     *
+     * The following special map event `type` could be registerd:
+     * - *mousedown*
+     * - *mouseup*
+     * - *click*
+     * - *dblclick*
+     * - *mousemove*
+     * - *mouseenter* / *mouseover*: the cursor enters a visible portion of the specified layer from outside that layer or outside the map canvas.
+     * - *mouseleave*: the cursor leaves a visible portion of the specified layer or leaves the map canvas.
+     * - *mouseout*: the cursor leaves the map canvas.
+     * - *contextmenu*
+     * - *touchstart*
+     * - *touchend*
+     * - *touchcancel*
+     *
+     * Events will be fired from **top** to **bottom** (following the [*bubble effect*](https://developer.mozilla.org/en-US/docs/Web/API/Event/bubbles))
+     * and only if the `originalEvent` can propagate (`bubbles` getter is falsy or `stopPropagation` has not been called on it).
+     * @param   {string}          type                     one of the available types.
+     * @param   {string|function} [layerIdOrListener=null] A style layer id if you want to restrict events to the visible features of a layer.
+     *
+     * Otherwise, a listener which takes a mapboxgl event as argument.
+     *
+     * The event object has three more properties:
+     * - `originalEvent`: the DOM event
+     * - `target`: the DOM target
+     * - `type`: the event type
+     * @param   {function}        [layerListener=null]     The listener if not defined in the previous parameter.
+     * @returns {Map} this
+     */
+    Map.prototype.on = function (type, layerIdOrListener = null, layerListener = null) {
+        let layerId = layerListener == null ? null : layerIdOrListener;
+        let listener = layerListener == null ? layerIdOrListener : layerListener;
+        if (isInteractiveEvent(type) && layerId) {
+            let layer = this.getLayer(layerId);
+            layer.metadata = layer.metadata || {};
+            listener.delegates = [];
+            if (type === "mouseenter" || type === "mouseover") {
+                let mousemove = (e) => {
+                    const layer = this.getLayer(layerId);
+                    const features = layer ? this.queryRenderedFeatures(e.point, { layers: [layerId] }) : [];
+                    if (!features.length) {
+                        layer.metadata["mouseenter:mousein"] = false;
+                    } else if (!layer.metadata["mouseenter:mousein"]) {
+                        layer.metadata["mouseenter:mousein"] = true;
+                        listener.call(this, Object.assign({ features, type }, e));
                     }
-                    e.originalEvent.stopPropagation();
-                }
+                };
+                mousemove.event = "mousemove";
+                let mouseout = () => {
+                    this.getLayer(layerId).metadata["mouseenter:mousein"] = false;
+                };
+                mouseout.event = "mouseout";
+                listener.delegates.push(mousemove);
+                listener.delegates.push(mouseout);
+            } else if (type === "mouseleave" || type === "mouseout") {
+                let mousemove = (e) => {
+                    const layer = this.getLayer(layerId);
+                    const features = layer ? this.queryRenderedFeatures(e.point, { layers: [layerId] }) : [];
+                    if (features.length) {
+                        layer.metadata["mouseleave:mousein"] = true;
+                    } else if (layer.metadata["mouseleave:mousein"]) {
+                        layer.metadata["mouseleave:mousein"] = false;
+                        listener.call(this, Object.assign({ type }, e));
+                    }
+                };
+                mousemove.event = "mousemove";
+                let mouseout = (e) => {
+                    if (layer.metadata["mouseleave:mousein"]) {
+                        layer.metadata["mouseleave:mousein"] = false;
+                        listener.call(this, Object.assign({ type }, e));
+                    }
+                };
+                mouseout.event = "mouseout";
+                listener.delegates.push(mousemove);
+                listener.delegates.push(mouseout);
+            } else {
+                let delegate = (e) => {
+                    const features = this.getLayer(layerId) ? this.queryRenderedFeatures(e.point, { layers: [layerId] }) : [];
+                    if (features.length) {
+                        listener.call(this, Object.assign({ features }, e));
+                    }
+                };
+                delegate.event = type;
+                listener.delegates.push(delegate);
             }
-        });
-        this.on("mouseleave", layerName, e => {
-            let featuresOnLayer = this.queryRenderedFeatures(e.point, { layers: [layerName] });
-            if (featuresOnLayer.length == 0) {
-                lastFeatureId = null;
-                exitCallback(e);
+            for (let delegate of listener.delegates) {
+                this.addListenerForLayer(layerId, delegate.event, delegate);
             }
-        });
+            return this;
+        } else {
+            return originalMapOnFunc.call(this, type, listener);
+        }
     };
 
     /**
-     * Create a click event on a layer.
+     * Removes an event listener (eventually for layer-specific events) previously added with [on method](#on).
+     * @param   {string}          type                     one of the available types.
+     * @param   {string|function} [layerIdOrListener=null] A style layer id from which you want to remove the listener.
      *
-     * `callback` functions is the same as for `on` map method.
-     *
-     * *Callback* is **optional** and have a new attribute **`feature`** which is just a shorthand for `features[0]`.
-     *
-     * The click event can only occurs on the layer with a feature with the greatest z-index.
-     * @alias module:mapboxgl-Map#onClick
-     * @param {string} layerName name of the layer
-     * @param {callback} callback callback method when a click happened on a feature on the layer
+     * Otherwise, the listener to remove.
+     * @param   {function}        [layerListener=null]     The listener to remove if not defined in the previous parameter.
+     * @returns {Map} this
      */
-    GLMap.prototype.onClick = function (layerName, callback = () => {}) {
-        this.on("click", layerName, e => {
-            if (!e.originalEvent.cancelBubble) {
-                addFeatureOnEvent(e);
-                let featureOnMap = this.queryRenderedFeatures(e.point)[0];
-                let featureOnLayer = e.feature;
-                if (getFeatureId(featureOnMap) == getFeatureId(featureOnLayer)) {
-                    callback(e);
-                    e.originalEvent.stopPropagation();
+    Map.prototype.off = function (type, layerIdOrListener = null, layerListener = null) {
+        let layerId = layerListener == null ? null : layerIdOrListener;
+        let listener = layerListener == null ? layerIdOrListener : layerListener;
+        if (isInteractiveEvent(type) && layerId) {
+            if (listener.delegates) {
+                for (let delegate of listener.delegates) {
+                    this.removeListenerFromLayer(layerId, delegate.event, delegate);
                 }
+            } else {
+                this.removeListenerFromLayer(layerId, type, listener);
             }
-        });
+            return this;
+        } else {
+            return originalMapOffFunc.call(this, type, listener);
+        }
     };
+
+    /**
+     * Fires an event of the specified type.
+     *
+     * Respect the *bubble effect*.
+     * @param   {string} type  one of the available types.
+     * @param   {object} event event data
+     * @returns {Map} this
+     * @see [`on` method](#on).
+     * @see [`Evented` on *mapboxgl*](https://www.mapbox.com/mapbox-gl-js/api/#evented).
+     */
+    Map.prototype.fire = function (type, event) {
+        if (isInteractiveEvent(type)) {
+            for (let layerEvented of this._getLayersEventedByZIndexFor(type)) {
+                layerEvented.fire(type, event);
+            }
+            if (event.originalEvent && event.originalEvent.cancelBubble) {
+                return this;
+            } else {
+                return originalMapFireFunc.call(this, type, event);
+            }
+        } else {
+            return originalMapFireFunc.call(this, type, event);
+        }
+    };
+
     return mapboxgl;
 }));
